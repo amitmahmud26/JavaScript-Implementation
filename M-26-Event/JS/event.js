8e59c9ae2c1109bbf5d7ea15d484a34e555db3f7
Amit Mahmud Sabbir <amitsclan26@gmail.com>
2023-08-19 01:31:44 +0600

    addEventListener

---
+++ M-26-Event/JS/event.js
@@ -0,0 +1,25 @@
+// console.log("this is a seperate event file")
+
+// option 1  directly write on the tag
+// option 2
+function makeRed(){
+    document.body.style.backgroundColor = "red";
+}
+
+// option 3  function on different place
+function makeBlue(){
+    document.body.style.backgroundColor = 'blue';
+}
+const makeBlueButton = document.getElementById('make-blue');
+makeBlueButton.onclick = makeBlue;
+
+// option 4  direct fucntion on the button
+const makePurpleButton = document.getElementById('make-purple');
+makePurpleButton.onclick = function(){
+    document.body.style.backgroundColor = 'purple';
+}
+
+// option 5 and final (addEventListener)
+document.getElementById('make-goldenrod').addEventListener('click', function(){
+    document.body.style.backgroundColor = 'goldenrod';
+})
